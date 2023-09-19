bda33a9f6ec454d3c3a729c52477bf5641068b58
BrettBeacon <brett.beacon@gmail.com>
2023-09-19 12:00:06 +1000

    Added chapters 14 and 15, still need to add in the changes for chapter 15 from canvas. Need to add the custom menu bar stuff.

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailFragment.kt
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailFragment.kt
@@ -7,10 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bignerdranch.android.criminalintent.databinding.FragmentCrimeDetailBinding
 import kotlinx.coroutines.launch
@@ -59,8 +61,12 @@ class CrimeDetailFragment : Fragment() {
                 }
             }
 
-            crimeDate.apply {
-                isEnabled = false
+            setFragmentResultListener(
+                DatePickerFragment.REQUEST_KEY_DATE
+            ) { _, bundle ->
+                val newDate =
+                    bundle.getSerializable(DatePickerFragment.BUNDLE_KEY_DATE) as Date
+                crimeDetailViewModel.updateCrime { it.copy(date = newDate) }
             }
 
             crimeSolved.setOnCheckedChangeListener { _, isChecked ->
@@ -82,6 +88,11 @@ class CrimeDetailFragment : Fragment() {
                 crimeTitle.setText(crime.title)
             }
             crimeDate.text = crime.date.toString()
+            crimeDate.setOnClickListener {
+                findNavController().navigate(
+                    CrimeDetailFragmentDirections.selectDate(crime.date)
+                )
+            }
             crimeSolved.isChecked = crime.isSolved
         }
     }
--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDao.kt
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDao.kt
@@ -1,7 +1,9 @@
 package com.bignerdranch.android.criminalintent.database
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.bignerdranch.android.criminalintent.Crime
 import java.util.UUID
 import kotlinx.coroutines.flow.Flow
@@ -13,4 +15,10 @@ interface CrimeDao {
 
     @Query("SELECT * FROM crime WHERE id=(:id)")
     suspend fun getCrime(id: UUID): Crime
+
+    @Update
+    suspend fun updateCrime(crime: Crime)
+
+    @Insert
+    suspend fun addCrime(crime: Crime)
 }
